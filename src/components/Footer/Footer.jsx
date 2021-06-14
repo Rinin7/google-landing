05d3b5b602417a3fabb23624dcc92a4518b5a09c
Ryan Doubinin <ryan_doubinin@hotmail.com>
2021-06-13 22:39:25 -0600

    Styled Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import "./Footer.scss";
 
 function Footer() {
   return (
     <div className="footer">
-      <h2 className="footer__header">footer</h2>
+      <div className="footer__country">Canada</div>
+      <div className="footer__links-container">
+        <div className="footer__links-info">
+          <a className="footer__links" href="#">
+            Advertising
+          </a>
+          <a className="footer__links" href="#">
+            Business
+          </a>
+          <a className="footer__links" href="#">
+            How Search Works
+          </a>
+        </div>
+        <div className="footer__links-legal">
+          <a className="footer__links" href="#">
+            Privacy
+          </a>
+          <a className="footer__links" href="#">
+            Terms
+          </a>
+          <a className="footer__links" href="#">
+            Settings
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
